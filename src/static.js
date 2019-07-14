71985f22d6d38c26d718eee92719e4ed97bd97db
Julien Mourer <getkey@getkey.eu>
2019-07-13 20:43:53 -0400

    Fix the canonicals

--- src/static.js
+++ src/static.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 
 const render = require('./lib/render.js');
 const { formatTitle, idFromDate } = require('./lib/url.js');
+const { baseUrl } = require('./config.js');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
@@ -28,6 +29,7 @@ fs.readdirSync(articlesDir)
 		const { content, excerpt, title } = render(md);
 		const id = idFromDate(date);
 		const articleDir = path.join(outDir, id);
+		const formatedTitle = formatTitle(title);
 
 		return Promise.all([
 			ejs.renderFile(path.join(__dirname, 'view', 'blog_render.ejs'), {
@@ -36,17 +38,17 @@ fs.readdirSync(articlesDir)
 				bodyCopy: content,
 				tags,
 				lang,
-				canonical: 'whatever',
+				canonical: `${baseUrl}/blog/${id}/${formatedTitle}`,
 			}),
 			mkdir(articleDir, { recursive: true }),
 		]).then(([html]) => ({
 			articleDir,
-			title,
+			formatedTitle,
 			html,
 			excerpt,
 		}));
-	}).then(({ articleDir, html, title }) => {
-		const formatedTitle = formatTitle(title);
-		return writeFile(path.join(articleDir, `${formatedTitle}.html`), html);
-	}));
+	}).then(({ articleDir, html, formatedTitle }) => writeFile(
+		path.join(articleDir, `${formatedTitle}.html`),
+		html,
+	)));
 // TODO: something with the excerpt
