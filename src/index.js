76f0bb6b3e8f2ea659a255b325b3bf39f70ba1ad
Julien Mourer <getkey@getkey.eu>
2019-07-14 22:32:02 -0400

    Remove stuff that's no longer needed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,149 @@
-const cli = require('./lib/cli.js');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const ejs = require('ejs');
 
-const subcommandPaths = {
-	start: './subcommands/start.js',
-	'regenerate-cache': './subcommands/regenerate_cache.js',
-	'set-password': './subcommands/generate_hash.js',
-};
-const panicString = `Argument expected: ${Object.keys(subcommandPaths).join('|')}`;
+const render = require('./lib/render.js');
+const { formatTitle, idFromDate } = require('./lib/url.js');
+const { baseUrl } = require('./config.js');
 
-if (process.argv.length < 3) cli.panic(panicString);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const mkdir = util.promisify(fs.mkdir);
 
-const subcommand = subcommandPaths[process.argv[2]];
-if (subcommand === undefined) cli.panic(panicString);
+const articlesDir = path.join(__dirname, '..', 'articles');
+const baseDir = path.join(__dirname, '..', 'public');
+const blogDir = path.join(baseDir, 'blog');
 
-const args = process.argv.slice(3);
-require(subcommand)(args);
+const articles = fs.readdirSync(articlesDir)
+	.map(dirName => Promise.all([
+		readFile(path.join(articlesDir, dirName, 'index.md'), 'utf8'),
+		readFile(path.join(articlesDir, dirName, 'metadata.json'), 'utf8').then(metadataStr => {
+			const metadata = JSON.parse(metadataStr);
+			metadata.date = new Date(metadata.date);
+			return metadata;
+		}),
+	]))
+	.map(promise => promise.then(([md, { tags, lang, date }]) => {
+		const { content, excerpt, title } = render(md);
+		return {
+			content,
+			excerpt,
+			title,
+			id: idFromDate(date),
+			tags,
+			lang,
+			date,
+			formatedTitle: formatTitle(title),
+		};
+	}));
+
+// blog articles
+articles.forEach(async promise => {
+	const { content, title, date, tags, lang, id, formatedTitle } = await promise;
+	const articleDir = path.join(blogDir, id);
+
+	const [html] = await Promise.all([
+		ejs.renderFile(path.join(__dirname, 'templates', 'blog_render.ejs'), {
+			title,
+			date,
+			bodyCopy: content,
+			tags,
+			lang,
+			canonical: `${baseUrl}/blog/${id}/${formatedTitle}`,
+		}),
+		mkdir(articleDir, { recursive: true }),
+	]);
+
+	writeFile(
+		path.join(articleDir, `${formatedTitle}.html`),
+		html,
+	);
+});
+
+const sortedArticles = Promise.all(articles).then(articles => ([
+	...articles,
+].sort((a, b) => b.date - a.date)));
+const mkBlogDir = mkdir(blogDir, { recursive: true });
+
+// blog index
+sortedArticles.then(async articles => {
+	const [html] = await Promise.all([
+		ejs.renderFile(path.join(__dirname, 'templates', 'blog_index.ejs'), {
+			postList: articles,
+			canonical: `${baseUrl}/blog`,
+		}),
+		mkBlogDir,
+	]);
+
+	writeFile(
+		path.join(blogDir, 'index.html'),
+		html,
+	);
+});
+
+// RSS feed
+sortedArticles.then(async articles => {
+	const [xml] = await Promise.all([
+		ejs.renderFile(path.join(__dirname, 'templates', 'atom.ejs'), {
+			postList: articles,
+		}),
+		mkBlogDir,
+	]);
+
+	writeFile(
+		path.join(blogDir, 'atom.xml'),
+		xml,
+	);
+});
+
+// about
+ejs.renderFile(path.join(__dirname, 'templates', 'about.ejs'), {
+	canonical: `${baseUrl}/about`,
+}).then(html => writeFile(
+	path.join(baseDir, 'about.html'),
+	html,
+));
+
+// 404
+ejs.renderFile(path.join(__dirname, 'templates', '404.ejs')).then(html => writeFile(
+	path.join(baseDir, '404.html'),
+	html,
+));
+
+// home
+ejs.renderFile(path.join(__dirname, 'templates', 'home.ejs'), {
+	canonical: baseUrl,
+}).then(html => writeFile(
+	path.join(baseDir, 'index.html'),
+	html,
+));
+
+sortedArticles.then(async articles => {
+	const articlesByTag = articles.reduce((acc, article) => {
+		article.tags.forEach(tag => {
+			if (acc[tag] === undefined) {
+				acc[tag] = [article];
+			} else {
+				acc[tag].push(article);
+			}
+		});
+		return acc;
+	}, {});
+
+	const tagDir = path.join(blogDir, 'tag');
+	await mkdir(tagDir, { recursive: true });
+
+	Object.entries(articlesByTag).forEach(async ([tag, articles]) => {
+		const html = await ejs.renderFile(path.join(__dirname, 'templates', 'blog_index.ejs'), {
+			postList: articles,
+			canonical: `${baseUrl}/blog/tag/${tag}`,
+			tag,
+		});
+
+		writeFile(
+			path.join(tagDir, `${tag}.html`),
+			html,
+		);
+	});
+});
